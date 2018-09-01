762e7f928973c1fcee3f39def00839adbf5a47c0
danielt <brightdanielt@gmail.com>
2018-09-01 16:55:45 +0800

    The callback for click on TargetTrackedItem
    Signed-off-by: danielt <brightdanielt@gmail.com>

---
+++ app/src/main/java/com/cauliflower/danielt/smartphoneradar/ui/TargetTrackedClickCallback.java
@@ -0,0 +1,7 @@
+package com.cauliflower.danielt.smartphoneradar.ui;
+
+import com.cauliflower.danielt.smartphoneradar.obj.RadarUser;
+
+public interface TargetTrackedClickCallback {
+    void onClick(RadarUser radarUser);
+}
