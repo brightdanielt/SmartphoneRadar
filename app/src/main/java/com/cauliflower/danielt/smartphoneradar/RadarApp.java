ce4b9dcc9c962dbe7c12389997658cccf572b261
danielt <brightdanielt@gmail.com>
2018-09-06 16:07:34 +0800

    方便取得 DataRepository
    Signed-off-by: danielt <brightdanielt@gmail.com>

--- app/src/main/java/com/cauliflower/danielt/smartphoneradar/RadarApp.java
+++ app/src/main/java/com/cauliflower/danielt/smartphoneradar/RadarApp.java
@@ -2,12 +2,27 @@ package com.cauliflower.danielt.smartphoneradar;
 
 import android.app.Application;
 
+import com.cauliflower.danielt.smartphoneradar.data.DataRepository;
 import com.cauliflower.danielt.smartphoneradar.data.RadarDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class RadarApp extends Application {
 
+    private ExecutorService executorService;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+         executorService = Executors.newSingleThreadExecutor();
+    }
 
     public RadarDatabase getDatabase() {
         return RadarDatabase.getInstance(this);
+    }
+
+    public DataRepository getRepository(){
+        return DataRepository.getInstance(getDatabase(),executorService);
     }
 }
